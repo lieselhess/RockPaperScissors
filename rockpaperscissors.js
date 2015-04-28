96435f273314042c980ecce7007383d56b33bf5d
Liesel Hess <lieselhess@Liesels-PKI-Laptop.local>
2015-04-28 14:15:50 -0500

    Adding experimental user input validation script

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,7 +1,14 @@
-/* Plays a Rock, Paper, Scissors game, human vs coputer, using a random number generator */
-
 var userChoice = prompt("Do you choose rock, paper, or scissors?");
 
+/* Validation to only allow rock, paper, scissors as input */
+
+if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+    console.log("You chose" + " " + userChoice);
+}
+else {
+    throw new Error("Please only enter rock, paper, or scissors!");
+   
+}
 var computerChoice = Math.random();
 
 console.log(computerChoice);
@@ -69,8 +76,14 @@ var compare = function(choice1, choice2) {
         }
         
     }
-    
+
+    else {
+
+        return "Please enter only 'rock', 'paper', or 'scissors'.";
+         }
     
 };
 
 compare(userChoice, computerChoice);
+
+
